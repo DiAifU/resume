74b02db884285c5f29a5e629bb0bd93622a2eab1
Nicolas Montvernay <nicolas.montvernay@gmail.com>
2019-02-20 01:54:06 +0100

    Made cv columns stackable on small screens

--- src/components/CVGrid.js
+++ src/components/CVGrid.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
 
 const CVGrid = ({ children, className }) => (
-    <Grid container divided relaxed padded className={className}>
+    <Grid container divided relaxed padded stackable className={className}>
     {children}
     </Grid>
 );
